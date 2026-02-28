3fde2f74ec828e3599548ba3ea888393e6b0bc72
MiguelP04 <miguel09112004@gmail.com>
2026-02-28 14:40:37 -0400

    fix: index in migrations added

--- db/migrate/20260227192510_create_variant_option_values.rb
+++ db/migrate/20260227192510_create_variant_option_values.rb
@@ -6,5 +6,8 @@ class CreateVariantOptionValues < ActiveRecord::Migration[7.2]
 
       t.timestamps
     end
+    add_index :variant_option_values, [:product_variant_id, :option_value_id],
+              unique: true,
+              name: "index_variant_option_uniqueness"
   end
 end
